19b5b1fa95512b58fee30f0ed7979c250fee10f5
Synx0 <games@rory.es>
2020-11-29 01:06:56 +0100

    PoolManager.cs refactored

--- Assets/Scripts/Pooling/PoolManager.cs
+++ Assets/Scripts/Pooling/PoolManager.cs
@@ -7,9 +7,7 @@ namespace Asteroids.Pooling
     public class PoolManager : MonoBehaviour
     {
         Dictionary<int, List<GameObject>> poolDictionary = new Dictionary<int, List<GameObject>>();
-
         static PoolManager _instance;
-
         List<Transform> _parents;
 
         public static PoolManager instance
@@ -47,54 +45,35 @@ namespace Asteroids.Pooling
         {
             int poolKey = prefab.GetInstanceID();
 
-            if (poolDictionary.ContainsKey(poolKey))
+            if (!poolDictionary.ContainsKey(poolKey)) return null;
+            List<GameObject> pool = poolDictionary[poolKey];
+
+            var pooledObject = pool.FirstOrDefault(x => !x.gameObject.activeInHierarchy);
+
+            if (pooledObject == null)
             {
-                List<GameObject> pool = poolDictionary[poolKey];
-
-                var obj = pool.FirstOrDefault(x => !x.gameObject.activeInHierarchy);
-
-                if (obj == null)
-                {
-                    obj = Instantiate(prefab, GetParentForPool(prefab));
-                    poolDictionary[poolKey].Add(obj);
-                }
-                obj.SetActive(true);
-                return obj;
+                pooledObject = Instantiate(prefab, GetParentForPool(prefab));
+                poolDictionary[poolKey].Add(pooledObject);
             }
-            return null;
+
+            pooledObject.SetActive(true);
+            return pooledObject;
         }
-
-        //public GameObject Spawn(GameObject prefab)
-        //{
-        //    int poolKey = prefab.GetInstanceID();
-
-        //    if (poolDictionary.ContainsKey(poolKey))
-        //    {
-        //        List<GameObject> pool = poolDictionary[poolKey];
-
-        //        var obj = pool.FirstOrDefault(x => !x.gameObject.activeInHierarchy);
-
-        //        if (obj == null)
-        //        {
-        //            obj = Instantiate(prefab, GetParentForPool(prefab));
-        //            poolDictionary[poolKey].Add(obj);
-        //        }
-        //        obj.SetActive(true);
-        //        return obj;
-        //    }
-        //    return null;
-        //}
 
         private Transform GetParentForPool(GameObject prefab)
         {
             Transform parent = _parents.FirstOrDefault(x => x.name == prefab.name);
             if (parent == null)
-            {
-                parent = new GameObject().transform;
-                parent.name = prefab.name;
-                parent.SetParent(this.transform);
-                _parents.Add(parent);
-            }
+                parent = CreateParentForPool(prefab);
+            return parent;
+        }
+
+        private Transform CreateParentForPool(GameObject prefab)
+        {
+            Transform parent = new GameObject().transform;
+            parent.name = prefab.name;
+            parent.SetParent(this.transform);
+            _parents.Add(parent);
             return parent;
         }
     }
